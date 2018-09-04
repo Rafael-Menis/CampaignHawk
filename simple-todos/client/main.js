33bdd80afebadeb4f300ff7d85390e423308ede2
Rafael Menis <rafael.menis@gmail.com>
2018-09-03 21:30:50 -0700

    Giving up on tsconfig as a red herring.

--- simple-todos/client/main.js
+++ simple-todos/client/main.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-
-import './main.html';
-
-import App from '../imports/ui/App.jsx';
+import 'main.html';
+import App from '../imports/ui/App.js';
 
 Meteor.startup(() => {
 	render(<App />, document.getElementById('render-target'));
--- simple-todos/imports/ui/App.js
+++ simple-todos/imports/ui/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Task from './Task.jsx';
+import Task from './Task';
 
 // App component, represents the whole app
 export default class App extends Component {
