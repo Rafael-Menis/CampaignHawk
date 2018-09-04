f4250e92a309c23e841516ef4272c2d590377242
Rafael Menis <rafael.menis@gmail.com>
2018-09-03 21:48:15 -0700

    Apparently,import  './main.html' stops the whole thing from working.

--- simple-todos/client/main.js
+++ simple-todos/client/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import 'main.html';
+
 import App from '../imports/ui/App.js';
 
 Meteor.startup(() => {
--- simple-todos/imports/ui/App.js
+++ simple-todos/imports/ui/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Task from './Task';
+import Task from './Task.js';
 
 // App component, represents the whole app
 export default class App extends Component {
