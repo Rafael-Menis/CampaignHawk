d0cea09cfc930b98ab956e82d434f5c624922b01
Rafael Menis <rafael.menis@gmail.com>
2018-09-28 00:22:27 -0700

    Filling out App a bit more.

--- CampaignHawk/client/components/App.jsx
+++ CampaignHawk/client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Map from './Map';
 import Login from './Login';
 import AppNotFound from './AppNotFound';
@@ -8,6 +8,14 @@ import { Meteor } from 'meteor/meteor';
 const App = () => (
     <Router>
     <div>
+        <ul>
+            <li>
+                <Link to="/">Map</Link>
+            </li>
+            <li>
+                <Link to="login">Login</Link>
+            </li>
+        </ul>
         <Switch>
             <Route exact path="/" component={Map} />
             <Route path="login" component={Login} />
--- CampaignHawk/client/main.js
+++ CampaignHawk/client/main.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
+
 import './main.html';
 
 Meteor.startup (() => {
