e19a31931943c736fcd1b9e8a524a69416b381a6
Rafael Menis <rafael.menis@gmail.com>
2018-09-29 15:52:42 -0700

    Finally stuff shows up, after 20 days of various attempts.

--- CampaignHawk/client/main.js
+++ CampaignHawk/client/main.js
@@ -4,11 +4,10 @@ import { Meteor } from 'meteor/meteor';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../imports/startup/Client/components/App';
 
-const rootElement = document.getElementById("root");
 Meteor.startup (() => {
-  ReactDOM.render ((
+  render ((
   <Router>
     <App />
   </Router>
-  ), rootElement);
+  ), document.getElementById("root"));
 });
