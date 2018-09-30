bae6d20292c8c21858f7dfc787d3eb7865a8f779
Rafael Menis <rafael.menis@gmail.com>
2018-09-30 10:06:50 -0700

    Sidebar online as a router component. Also changed some styles.

--- CampaignHawk/imports/startup/Client/components/Map.js
+++ CampaignHawk/imports/startup/Client/components/Map.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
+/* React Router Dom v4 allows for creating nav links with built in styling directly */
+
+const SideNav = () => (
+    <Router>
+        <div>
+         <ul>
+            <li>
+                items
+            </li>
+            <li>
+                test
+            </li>
+         </ul>
+        </div>
+    </Router>
+)
 const Map = () => (
+    <Router>
+    <div>
+    <SideNav />
+   
     <h1>This is where the map goes</h1>
+    </div>
+    </Router>
 )
 
 export default Map
