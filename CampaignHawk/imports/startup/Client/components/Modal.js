1ae3888e23dcd999c7860766920237b7b5b1d631
Rafael Menis <rafael.menis@gmail.com>
2018-10-04 21:06:45 -0700

    Initial modal setup- getting the shell styles online.

--- CampaignHawk/imports/startup/Client/components/Modal.js
+++ CampaignHawk/imports/startup/Client/components/Modal.js
@@ -1,5 +1,39 @@
-<div className="modal-active-darken">
-    <div className="modal-container">
-        
-    </div>
-</div>
+import React, { Component } from 'react';
+
+export default class Modal extends Component {
+    render() {
+        return (
+            <div className="modal-active-darken">
+                <div className="modal-container">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default class AddVolunteerModalContent extends Component {
+    render () {
+        return (
+            <div>
+                <div className="volunteer-form-row">
+                    <div className="volunteer-form-column-55">
+
+                    </div>
+                    <div className="volunteer-form-column-45">
+
+                    </div>
+                </div>
+                <div className="volunteer-form-row">
+                    <div className="volunteer-form-column-100">
+
+                    </div>
+                </div>
+                <div className="submit-modal-buttons">
+                    
+                </div>
+            </div>
+        )
+    }
+}
